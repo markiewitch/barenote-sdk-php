ce0690904b3499ff3dadc886c3badfa288f073b0
Michał Markiewicz <mmarkiewicz@gog.com>
2017-01-28 22:05:57 +0100

    update PHPDoc as per Scrutinizer advice

--- src/Barenote/Domain/Identity/NoteId.php
+++ src/Barenote/Domain/Identity/NoteId.php
@@ -7,13 +7,23 @@ namespace Barenote\Domain\Identity;
  */
 class NoteId
 {
+    /**
+     * @var int
+     */
     private $value;
 
+    /**
+     * NoteId constructor.
+     * @param int $value
+     */
     public function __construct($value)
     {
         $this->value = $value;
     }
 
+    /**
+     * @return int
+     */
     public function getValue()
     {
         return $this->value;
